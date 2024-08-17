86e7f1926cc4519dec07bcc1eba4583e8c1699dc
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-08-17 19:10:34 +0500

    create password generator with greate stack vedio

---
+++ app.js
@@ -0,0 +1,30 @@
+const passwordInput = document.getElementById("password");
+const copyText = document.getElementById("copyText");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_+-*/<>?/=.,:;~";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+const createPassword = () => {
+    copyText.innerText = ""
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordInput.value = password
+};
+
+const copyPassword = () => {
+    passwordInput.select();
+    document.execCommand("copy")
+    copyText.innerText = "Copy Text"
+}
